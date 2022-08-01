abb0e51911875df22ac1ec0f25739da584d64971
YoungLights <hello@younglights.de>
2022-08-01 16:49:54 +0200

    Setup the project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+	<main>
+		
+		<div class="title">
+			<h1>Choose your warrior</h1>
+			<p>and join the squad</p>
+			<button @click="toggleModal">follow your path</button>
+		</div>
+		
+		<div class="modal" :class="{active: stateModal}">
+			<div class="modal-inner">
+				<div class="modal-close" @click="toggleModal">x</div>
+				<h4>Your name</h4>
+				<input type="text">
+				<button>enter your destiny</button>
+			</div>
+		</div>
+
+		<div class="squad-list">
+			<div class="squad-item">
+				<h4>{{name}}</h4>
+				
+			</div>
+			
+		</div>
+
+	</main>
+</template>
+
+<script setup>
+	import {ref} from 'vue'
+
+	const stateModal = ref(false)
+	const name = ref(null)
+
+	const toggleModal = () => {
+		stateModal.value = !stateModal.value
+	}
+</script>
